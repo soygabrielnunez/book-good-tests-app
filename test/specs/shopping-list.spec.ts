9fedef0827d8ca006ad0c3a74ac1e00d6c16a132
gnunez <gnunez@aidacanarias.com>
2025-01-15 08:29:32 +0000

    test: 🧪

---
+++ test/specs/shopping-list.spec.ts
@@ -0,0 +1,7 @@
+import { it } from '@application-test-utils'
+
+it('should be possible to add items to the list', async ({ driver }) => {
+  // I open the shopping list
+  // I add an item to the shopping list
+  // I expect the item to be on the shopping list
+})
