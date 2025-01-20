bcec812e6e6b4c442a928cc34d078ba77c57eefa
gnunez <gnunez@aidacanarias.com>
2025-01-20 07:39:06 +0000

    refactor: ♻️ Extract logic to a component

---
+++ src/components/ShoppingList.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <form @submit.prevent="handleSubmit">
+      <label>Item
+        <input type="text" v-model="itemTitle" />
+      </label>
+      <button>Add item</button>
+    </form>
+
+    <ul>
+      <li v-for="item in items" :key="item.id">
+        {{ item.title }}
+      </li>
+    </ul>
+  </div>
+</template>
+<script setup lang="ts">
+import type { Item } from 'src/entities/shopping-list';
+import { ref } from 'vue';
+
+const SHOPPING_LIST_KEY = 'shopping-list';
+const items = ref<Item[]>(JSON.parse(localStorage.getItem(SHOPPING_LIST_KEY) || '[]'));
+
+const updateStorage = () => {
+  localStorage.setItem(SHOPPING_LIST_KEY, JSON.stringify(items.value));
+};
+
+const addItem = (item: Item) => {
+  items.value.push(item);
+  updateStorage();
+};
+
+const itemTitle = ref('');
+
+const handleSubmit = () => {
+  addItem({ id: Date.now(), title: itemTitle.value });
+  itemTitle.value = '';
+};
+</script>
