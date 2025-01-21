cd1336a97d4cd9504f0303d750554cf1fa9f98af
gnunez <gnunez@aidacanarias.com>
2025-01-21 08:09:01 +0000

    feat: 🚧

--- test/specs/shopping-list.spec.ts
+++ test/specs/shopping-list.spec.ts
@@ -7,3 +7,11 @@ it('should be possible to add items to the list', async ({ driver }) => {
   await shoppingList.addItem('Apples')
   await shoppingList.expectItemToBeOnList('Apples')
 })
+
+it('should be possible to remove items', async ({ driver }) => {
+  const shoppingList = makeShoppingList({ driver })
+  await shoppingList.open()
+  await shoppingList.addItem('Apples')
+  // remove the item from the shopping list
+  // expect the item not to be in the shopping list
+})
