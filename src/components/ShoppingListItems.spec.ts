94b4745a68ea03f5d656368fdcedba9e9f8b1bcf
gnunez <gnunez@aidacanarias.com>
2025-01-22 08:11:31 +0000

    test: 🧪 should emit a remove event when clicking an item

--- src/components/ShoppingListItems.spec.ts
+++ src/components/ShoppingListItems.spec.ts
@@ -1,6 +1,22 @@
-import { it, setup } from '../../test/utils'
+import { expect, it, screen, setup } from '../../test/utils'
 import ShoppingListItems from "./ShoppingListItems.vue";
 
-it('should emit a remove event when clicking an item', () => {
-  const { emitted, user } = setup(ShoppingListItems)
+it('should emit a remove event when clicking an item', async () => {
+  const { emitted, user } = setup(ShoppingListItems, {
+    renderOptions: {
+      props: {
+        items: [
+          { id: 1, title: 'Item 1' },
+          { id: 2, title: 'Item 2' },
+          { id: 3, title: 'Item 3' }
+        ]
+      }
+    }
+  })
+
+  const removeFirstItemButton = await screen.findByRole('button', { name: 'Remove Item 1' })
+  await user.click(removeFirstItemButton)
+
+  const removeEvent = emitted().remove as unknown[][]; 
+  expect(removeEvent[0][0]).toEqual({ id: 1, title: 'Item 1' })
 })
