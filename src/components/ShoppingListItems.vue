e9bc1b1056cada90cee4657a43173e299d7625fc
gnunez <gnunez@aidacanarias.com>
2025-01-23 08:20:21 +0000

    test: ✅ should emit a remove event when clicking an item

--- src/components/ShoppingListItems.vue
+++ src/components/ShoppingListItems.vue
@@ -1,3 +1,19 @@
 <template>
-  <div />
+  <ul>
+    <li v-for="item in items" :key="item.id">
+      {{ item.title }}
+      <button @click="emits('remove', item)">Remove {{ item.title }}</button>
+    </li>
+  </ul>
 </template>
+<script setup lang="ts">
+import type { Item } from 'src/entities/shopping-list';
+
+defineProps<{
+ items: Item[];
+}>();
+
+const emits = defineEmits<{
+ (event: 'remove', item: Item): void;
+}>();
+</script>
