a55a4d389f706344533e1d3fe2b5bc52640c6bb2
gnunez <gnunez@aidacanarias.com>
2025-01-29 08:19:34 +0000

    test: 🧪 should emit an edit event when the user edits and save an item

--- src/components/ShoppingListItems.spec.ts
+++ src/components/ShoppingListItems.spec.ts
@@ -20,3 +20,29 @@ it('should emit a remove event when clicking an item', async () => {
   const removeEvent = emitted().remove as unknown[][]; 
   expect(removeEvent[0][0]).toEqual({ id: 1, title: 'Item 1' })
 })
+
+it('should emit an edit event when the user edits and save an item', async () => {
+  const { emitted, user } = setup(ShoppingListItems, {
+    renderOptions: {
+      props: {
+        items: [
+          { id: 1, title: 'Item 1' },
+          { id: 2, title: 'Item 2' },
+          { id: 3, title: 'Item 3' }
+        ]
+      }
+    }
+  })
+
+  const editFirstItemButton = await screen.findByRole('button', { name: 'Edit Item 1' })
+  await user.click(editFirstItemButton)
+
+  const input = await screen.findByLabelText('New item')
+  await user.type(input, ' - Edited')
+
+  const saveButton = await screen.findByRole('button', { name: 'Save' })
+  await user.click(saveButton)
+
+  const editEvent = emitted().edit as unknown[][]; 
+  expect(editEvent[0][0]).toEqual({ id: 1, title: 'Item 1 - Edited' })
+})
